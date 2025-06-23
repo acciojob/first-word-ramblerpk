3c71967d5d03c4daece9ca2fee9391e7f5e79ffd
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-06-23 17:57:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function firstWord(s) {
   // your code here
-	if(s.length===0){
-		return '';
-	}
-	let spaceIndex = s.indexOf(' ');
-	if(spaceIndex !== -1){
-		return s.substring(0, spaceIndex);
-	}
-	else {
-		return s;
-	}
+	if (s.length === 0) {
+        return '';
+    }
+
+    // Find the index of the first space
+    let spaceIndex = s.indexOf(' ');
+
+    // Check if there is a space
+    if (spaceIndex !== -1) {
+        // Return substring up to the first space
+        return s.substring(0, spaceIndex);
+    } else {
+        // Return the entire string if no space
+        return s;
+    }
 }
 
 // Do not change the code below
